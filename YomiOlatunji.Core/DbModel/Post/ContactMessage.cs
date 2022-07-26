f0a42d1569b690f417968eb1dc0215f34cacc167
Yomi Olatunji <mosesoluwayomi@gmail.com>
2022-07-26 17:33:13 +0100

    Update Pages

--- YomiOlatunji.Core/DbModel/Post/ContactMessage.cs
+++ YomiOlatunji.Core/DbModel/Post/ContactMessage.cs
@@ -9,17 +9,24 @@ namespace YomiOlatunji.Core.DbModel.Post
         public long Id { get; set; }
 
         [MaxLength(200)]
-        public string Name { get; set; }
+        [Display(Name = "Name", Order = -9, Prompt = "Enter Your Full Name", Description = "Full Name")]
+        public string? Name { get; set; }
 
         [MaxLength(200)]
+        [Required]
+        [Display(Name = "Email Address", Order = -9,Prompt = "Enter Your Email Address", Description = "Email Address")]
         public string EmailAddress { get; set; }
 
         [MaxLength(100)]
+        [Display(Name = "Phone Number", Order = -9, Prompt = "Enter Your Phone Number", Description = "Phone Number")]
         public string? PhoneNumber { get; set; }
 
         [MaxLength(400)]
-        public string Title { get; set; }
+        [Display(Name = "Subject", Order = -9, Prompt = "Type your message subject", Description = "Message Subject")]
+        public string? Title { get; set; }
 
+        [Required]
+        [Display(Name = "Message", Order = -9, Prompt = "Type your message here", Description = "Message")]
         public string Message { get; set; }
 
         public bool? Read { get; set; }
